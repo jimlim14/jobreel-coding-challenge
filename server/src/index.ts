eb7f804d4efe4fb58a5519f5534bd37784a205f1
Jim <limchorngjim@gmail.com>
2024-01-24 17:17:15 +0100

    setup cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,14 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 const router = require('./router');
+const cors = require('cors');
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.json()).use(router)
+app.use(cors()).use(express.json()).use(router)
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
