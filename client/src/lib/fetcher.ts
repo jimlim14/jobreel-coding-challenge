9c6e0845ce762244a8fa79dac37afe3be3868e02
Jim <limchorngjim@gmail.com>
2024-01-25 10:34:27 +0100

    implement fetcher function

---
+++ client/src/lib/fetcher.ts
@@ -0,0 +1,27 @@
+export default async function fetcher(
+	url: string,
+	method: string | undefined = undefined,
+	data = undefined,
+) {
+	try {
+		const res = await fetch(`http://localhost:3000${url}`, {
+			method: method ? method : "GET",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify(data),
+		});
+		if (!res.ok) {
+			throw new Error("Request failed");
+		}
+		return {
+			data: await res.json(),
+			error: null,
+		};
+	} catch (e: any) {
+		return {
+			data: null,
+			error: e.message || "Something went wrong",
+		};
+	}
+}
