119b1757cc83057933ca8cbac33bd7f9cc9a456b
Jim <limchorngjim@gmail.com>
2024-01-28 21:46:41 +0800

    create reusable input and textarea field

---
+++ client/src/components/InputField.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const InputField: React.FC<Props> = ({ ...props }) => {
+	return (
+		<input
+			className="w-full outline-none rounded-lg border-2 border-gray-200 bg-white focus:border-blue-600 p-3 text-sm  text-gray-900"
+			{...props}
+		/>
+	);
+};
+
+export default InputField;
---
+++ client/src/components/TextArea.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+interface Props extends React.TextareaHTMLAttributes<HTMLTextAreaElement> {}
+
+const TextArea: React.FC<Props> = ({ ...props }) => {
+	return (
+		<textarea
+			className="w-full outline-none rounded-lg border-2 border-gray-200 bg-white text-left focus:border-blue-600 p-3 text-sm  text-gray-900"
+			{...props}
+		/>
+	);
+};
+
+export default TextArea;
