dadb9f55187315be984bb61117f9497e5723dd62
Jim <limchorngjim@gmail.com>
2024-01-29 13:11:20 +0800

    add toast to successfully created resume

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -2,12 +2,25 @@ import Image from "next/image";
 import { useState, useEffect } from "react";
 import { IResume } from "@/interfaces/interfaces";
 import Button from "@/components/Button";
-import Resume from "@/components/Resume";
 import fetcher from "@/lib/fetcher";
 import Link from "next/link";
 import ResumePreview from "@/components/ResumePreview";
+import { ToastContainer, toast, Bounce } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Home() {
+	const notify = () =>
+		toast.success("Successfully created a resume!", {
+			position: "bottom-right",
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "light",
+			transition: Bounce,
+		});
 	const [resumes, setResumes] = useState<IResume[]>([]);
 	const [error, setError] = useState<string | null>(null);
 
@@ -30,13 +43,16 @@ export default function Home() {
 	async function addResume() {
 		const { data, error } = await fetcher("/resumes", "POST");
 		setResumes((prevResumes) => [...prevResumes, data]);
+		notify();
 	}
 
 	return (
 		<div className="px-6 mx-auto w-full max-w-screen-xl">
 			<div className="flex justify-between items-center py-6">
 				<p className="text-2xl">My resumes</p>
-				<Button onClick={addResume}>Add resume</Button>
+				<Button onClick={addResume} variation="primary">
+					Add resume
+				</Button>
 			</div>
 			<div className="grid md:grid-cols-2 xl:grid-cols-4 gap-6">
 				{resumes &&
@@ -48,6 +64,7 @@ export default function Home() {
 						</Link>
 					))}
 			</div>
+			<ToastContainer />
 		</div>
 	);
 }
